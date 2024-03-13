bcc4cc211928e3b30e5afa197e0e1f87807b4ad1
nagendra <nagendrasha191@gmail.com>
2024-03-13 15:54:37 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.2.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Blui</title>
+</head>
+
+<body>
+    <!-- main-nav -->
+    <nav class="navbar navbar-expand-lg bg-white d-flex align-items-center">
+        <div class="container-fluid px-5 g-0">
+            <a class="navbar-brand logo" href="#">blui</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="searchbar mx-2">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2 search-input" type="search" placeholder="Search"
+                        aria-label="Search">
+                </form>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex align-items-center">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Offer Zone <span
+                                style="margin-left: 10px;">|</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"">Become A Seller <span style=" margin-left: 10px;">|</span></a>
+                    </li>
+                    <li class="nav-item text-center">
+                        <a class="nav-link icon-link" href="#"><i class="bi bi-person-fill"></i><br>Profile</a>
+                    </li>
+                    <li class="nav-item text-center">
+                        <a class="nav-link icon-link" href="#"><i class="bi bi-heart-fill"></i><br>Wishlist</a>
+                    </li>
+                    <li class="nav-item text-center">
+                        <a class="nav-link icon-link" href="#"><i class="bi bi-cart-fill"></i><br>Cart</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- main-nav end -->
+
+    <div class="container-fluid px-5">
+        <nav class="navbar main-nav navbar-expand-lg navbar-light bg-white">
+            <div class="container-fluid w-100 d-flex justify-content-center">
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav justify-content-evenly">
+                        <li class="nav-item">
+                            <a class="nav-link px-3" aria-current="page" href="#">Men</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Women</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Kids</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home & Kithens</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Beauty & Health</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Jewellery</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Bags & Footwear</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Electronics</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <!-- banner section -->
+
+    <div class="container mt-4 mx-5">
+        <div class="row">
+            <div class="col-10">
+                <img src="./images/banner.png" alt="banner" width="1111px" height="360px">
+            </div>
+            <div class="col-2">
+                <div class="card login-ui p-2">
+                    <div class="row">
+                        <div class="col-3"><img src="./images/Group 427319124.png" alt=""></div>
+                        <div class="col-9">
+                            <p>Hi user <br>let's Get Started </p>
+                        </div>
+                    </div>
+                    <a href="#" class="btn btn-primary btn-sm mb-2">Register</a>
+                    <a href="#" class="btn btn-light btn-sm">Login</a>
+                </div>
+                <div class="card quotes mt-2 p-3">
+                    <p>Send quotes with <br> supplier <br> preferences</p>
+                </div>
+                <div class="card discount mt-2 p-3">
+                    <p>Send quotes with <br> supplier <br> preferences</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- banner end  -->
+
+    <!-- trending category -->
+
+    <div class="container-fluid trending mt-4">
+        <div class="container text-center">
+            <h1>Trending Category</h1>
+            <div class="row pb-3">
+                <div class="col box-t">
+                    <h2>Men Fashion</h2>
+                    <div class="row mt-4">
+                        <div class="col box-child-1">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col box-child-2">
+                            <p>Topwear</p>
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col box-child-3">
+                            <p>Footwear</p>
+                        </div>
+                        <div class="col box-child-4">
+                            <p>Accessories</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col box-t">
+                    <h2>Women Fashion</h2>
+                    <div class="row mt-4">
+                        <div class="col box-child-1">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col box-child-2">
+                            <p>Topwear</p>
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col box-child-1">
+                            <p>Footwear</p>
+                        </div>
+                        <div class="col box-child-2">
+                            <p>Accessories</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col box-t">
+                    <h2>Kids Fashion</h2>
+                    <div class="row mt-4">
+                        <div class="col box-child-1">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col box-child-2">
+                            <p>Topwear</p>
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col box-child-1">
+                            <p>Footwear</p>
+                        </div>
+                        <div class="col box-child-2">
+                            <p>Accessories</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- trending category end -->
+
+    <div class="container-fluid decor mt-4">
+        <div class="container text-center">
+            <div class="row pb-3 pt-5">
+                <div class="col box">
+                    <h2>Home & Decor</h2>
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col box">
+                    <h2>Health & Beauty</h2>
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                        <div class="col-4 box-child">
+                            <p>Topwear</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- best deals container -->
+    <div class="container-fluid">
+        <!-- header -->
+        <div class="row px-2">
+            <div class="col-5 d-flex align-items-center">
+                <h2 style="font-size: 24px;font-weight: 600; margin-left: 30px;">Best Deals</h2>
+                <p class="ms-4 mt-3" style="font-weight: 400; font-size: 14px;">Deals ends in</p>
+                <div class="timer ms-3">12m 18h</div>
+            </div>
+            <div class="col-6">
+                <a href="#" class="btn btn float-end">Browse All Product <i class="bi bi-arrow-right"></i></a>
+            </div>
+        </div>
+        <!-- header end -->
+        <div class="row px-4 d-flex justify-content-center">
+            <div class="col-2 big-product border">
+                <img class="mt-2" src="./images/Frame48095492.png" alt="">
+                <div class="rating">
+                    <img src="./images/rating.png" alt="">
+                    <p>(52,677)</p>
+                </div>
+                <p style="font-size: 14px;margin-top: -10px;">Deepika Padukone Red Embroidery Wedding & Reception Saree
+                    with Blouse</p>
+                <span style="font-size: 20px;font-weight: 700;"><del
+                        style="font-size: 15px;font-weight: 700;">₹2999</del>₹1499</span>
+                <p style="font-weight: 400px;font-size: 14px;line-height: 20px;">Deepika Padukone has Red Saree is Her
+                    Wedding. So Beautifull Saree with Blouse & Setup. are so Availability Off Stock & Update</p>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-2 big-wishlist d-flex align-items-center">
+                        <a href="#"><img src="./images/Heart.png" alt=""></a>
+                    </div>
+                    <div class="col-8 big-cartbtn text-center d-flex align-items-center">
+                        <a href="#" class="btn btn text-white"><i class="bi bi-cart-fill"></i> Add to Cart</a>
+                    </div>
+                    <div class="col-2 big-wishlist d-flex align-items-center">
+                        <a href="#"><img src="./images/Eye.png" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="row">
+                    <?php for( $x=0; $x<=9; $x++ ){ ?>
+                    <div class="col-2 small-product border">
+                        <div class="pd-img">
+                            <img class="mt-2" src="./images/model1.png" alt="">
+                            <a href="#" class="btn btn-off d-flex align-items-center">50% Off</a>
+                            <!-- <a href="#" class="btn btn-sold d-flex align-items-center">Sold Out</a> -->
+                        </div>
+                        <p style="font-weight: 400px;font-size: 13px;">Pink Banarasi Silk Beautifull Saree with Blouse
+                        </p>
+                        <div class="d-flex">
+                            <!-- <img class="rating-icon" src="./images/rating.png" alt=""> -->
+                            <span style="font-size: 14px;font-weight: 700;"><del
+                                    style="font-size: 10px;font-weight: 700;">₹2999</del>₹1499</span>
+                        </div>
+                    </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- best deals container end -->
+
+    <!-- deals of the day -->
+    <div class="container-fluid mt-5 dealod">
+        <!-- header -->
+        <div class="row px-2">
+            <div class="col-5 d-flex align-items-center">
+                <h2 style="font-size: 24px;font-weight: 600; margin-left: 30px;">Deals of the Day</h2>
+                <p class="ms-4 mt-3" style="font-weight: 400; font-size: 14px;">Deals ends in</p>
+                <div class="timer ms-3">12m 18h</div>
+            </div>
+            <div class="col-6">
+                <a href="#" class="btn btn float-end">Browse All Product <i class="bi bi-arrow-right"></i></a>
+            </div>
+        </div>
+        <!-- header end -->
+        <div class="row px-4 d-flex justify-content-center">
+            <div class="col-2 big-product border">
+                <img class="mt-2" src="./images/Frame48095492.png" alt="">
+                <div class="rating">
+                    <img src="./images/rating.png" alt="">
+                    <p>(52,677)</p>
+                </div>
+                <p style="font-size: 14px;margin-top: -10px;">Deepika Padukone Red Embroidery Wedding & Reception Saree
+                    with Blouse</p>
+                <span style="font-size: 20px;font-weight: 700;"><del
+                        style="font-size: 15px;font-weight: 700;">₹2999</del>₹1499</span>
+                <p style="font-weight: 400px;font-size: 14px;line-height: 20px;">Deepika Padukone has Red Saree is Her
+                    Wedding. So Beautifull Saree with Blouse & Setup. are so Availability Off Stock & Update</p>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-2 big-wishlist d-flex align-items-center">
+                        <a href="#"><img src="./images/Heart.png" alt=""></a>
+                    </div>
+                    <div class="col-8 big-cartbtn text-center d-flex align-items-center">
+                        <a href="#" class="btn btn text-white"><i class="bi bi-cart-fill"></i> Add to Cart</a>
+                    </div>
+                    <div class="col-2 big-wishlist d-flex align-items-center">
+                        <a href="#"><img src="./images/Eye.png" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="row">
+                    <?php for( $x=0; $x<=9; $x++ ){ ?>
+                    <div class="col-2 small-product border">
+                        <div class="pd-img">
+                            <img class="mt-2" src="./images/model1.png" alt="">
+                            <a href="#" class="btn btn-off d-flex align-items-center">50% Off</a>
+                            <!-- <a href="#" class="btn btn-sold d-flex align-items-center">Sold Out</a> -->
+                        </div>
+                        <p style="font-weight: 400px;font-size: 13px;">Pink Banarasi Silk Beautifull Saree with Blouse
+                        </p>
+                        <div class="d-flex">
+                            <!-- <img class="rating-icon" src="./images/rating.png" alt=""> -->
+                            <span style="font-size: 14px;font-weight: 700;"><del
+                                    style="font-size: 10px;font-weight: 700;">₹2999</del>₹1499</span>
+                        </div>
+                    </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- deals of the day end -->
+
+
+</body>
+<script src="./bootstrap-5.2.3-dist/js/bootstrap.min.js"></script>
+
+</html>
